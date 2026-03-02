dda5687a17c1ef32c9db1a96f44e93891c423cc4
Marina Sevkovich <slolkunchik@gmail.com>
2026-03-02 15:32:06 +0300

    feat: add nof found page

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,4 +1,5 @@
 import { eachDayOfInterval } from "date-fns";
+import { notFound } from "next/navigation";
 
 import { supabase } from "./supabase";
 
@@ -17,6 +18,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
